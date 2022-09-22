1283b4155bef0fbd13f4e6b5dd9ed09d1bd16a93
Beatriz Angélica <92961502+angelicabeatriz@users.noreply.github.com>
2022-09-21 21:44:08 -0300

    Alterando tela dos formularios

--- Trabalho/Apresentacao/Cadastro.Designer.cs
+++ Trabalho/Apresentacao/Cadastro.Designer.cs
@@ -35,84 +35,112 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(76, 50);
+            this.label1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(29, 78);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.Size = new System.Drawing.Size(38, 16);
             this.label1.TabIndex = 0;
             this.label1.Text = "Login";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(71, 95);
+            this.label2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(29, 135);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.Size = new System.Drawing.Size(44, 16);
             this.label2.TabIndex = 1;
             this.label2.Text = "Senha";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(24, 130);
+            this.label3.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(29, 192);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(85, 13);
+            this.label3.Size = new System.Drawing.Size(103, 16);
             this.label3.TabIndex = 2;
             this.label3.Text = "Confirmar Senha";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(128, 174);
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(183, 252);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 3;
             this.button1.Text = "Cadastrar";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(115, 47);
+            this.textBox1.Location = new System.Drawing.Point(32, 97);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.Size = new System.Drawing.Size(226, 20);
             this.textBox1.TabIndex = 4;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(115, 92);
+            this.textBox2.Location = new System.Drawing.Point(32, 154);
             this.textBox2.Name = "textBox2";
             this.textBox2.PasswordChar = '*';
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.Size = new System.Drawing.Size(226, 20);
             this.textBox2.TabIndex = 5;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(115, 127);
+            this.textBox3.Location = new System.Drawing.Point(32, 211);
             this.textBox3.Name = "textBox3";
             this.textBox3.PasswordChar = '*';
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.Size = new System.Drawing.Size(226, 20);
             this.textBox3.TabIndex = 6;
             // 
-            // Form2
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(12, 23);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(345, 328);
+            this.panel1.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Black", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(27, 18);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(121, 27);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Cadastrar ";
+            // 
+            // Cadastro
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(468, 314);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(450, 434);
+            this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
-            this.Name = "Form2";
+            this.Name = "Cadastro";
             this.Text = "Cadastro";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -125,5 +153,7 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label4;
     }
 }
--- Trabalho/Apresentacao/Login.Designer.cs
+++ Trabalho/Apresentacao/Login.Designer.cs
@@ -37,15 +37,18 @@
             this.btCadastro = new System.Windows.Forms.Button();
             this.btSair = new System.Windows.Forms.Button();
             this.btEntrar = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lbFacaLogin = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(24, 29);
+            this.pictureBox1.Location = new System.Drawing.Point(21, 47);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(153, 124);
+            this.pictureBox1.Size = new System.Drawing.Size(102, 75);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -53,88 +56,116 @@
             // lbLogin
             // 
             this.lbLogin.AutoSize = true;
-            this.lbLogin.Location = new System.Drawing.Point(86, 206);
+            this.lbLogin.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbLogin.Location = new System.Drawing.Point(18, 137);
             this.lbLogin.Name = "lbLogin";
-            this.lbLogin.Size = new System.Drawing.Size(33, 13);
+            this.lbLogin.Size = new System.Drawing.Size(38, 16);
             this.lbLogin.TabIndex = 1;
             this.lbLogin.Text = "Login";
             // 
             // lbSenha
             // 
             this.lbSenha.AutoSize = true;
-            this.lbSenha.Location = new System.Drawing.Point(81, 242);
+            this.lbSenha.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbSenha.Location = new System.Drawing.Point(18, 192);
             this.lbSenha.Name = "lbSenha";
-            this.lbSenha.Size = new System.Drawing.Size(38, 13);
+            this.lbSenha.Size = new System.Drawing.Size(44, 16);
             this.lbSenha.TabIndex = 2;
             this.lbSenha.Text = "Senha";
             // 
             // tbLogin
             // 
-            this.tbLogin.Location = new System.Drawing.Point(125, 203);
+            this.tbLogin.Location = new System.Drawing.Point(21, 156);
             this.tbLogin.Name = "tbLogin";
-            this.tbLogin.Size = new System.Drawing.Size(100, 20);
+            this.tbLogin.Size = new System.Drawing.Size(237, 20);
             this.tbLogin.TabIndex = 3;
             this.tbLogin.TextChanged += new System.EventHandler(this.tbLogin_TextChanged);
             // 
             // tbSenha
             // 
-            this.tbSenha.Location = new System.Drawing.Point(125, 239);
+            this.tbSenha.Location = new System.Drawing.Point(21, 211);
             this.tbSenha.Name = "tbSenha";
             this.tbSenha.PasswordChar = '*';
-            this.tbSenha.Size = new System.Drawing.Size(100, 20);
+            this.tbSenha.Size = new System.Drawing.Size(237, 20);
             this.tbSenha.TabIndex = 4;
             this.tbSenha.TextChanged += new System.EventHandler(this.tbSenha_TextChanged);
             // 
             // btCadastro
             // 
-            this.btCadastro.Location = new System.Drawing.Point(136, 274);
+            this.btCadastro.BackColor = System.Drawing.Color.White;
+            this.btCadastro.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btCadastro.Location = new System.Drawing.Point(102, 275);
             this.btCadastro.Name = "btCadastro";
             this.btCadastro.Size = new System.Drawing.Size(75, 23);
             this.btCadastro.TabIndex = 5;
             this.btCadastro.Text = "Cadastro";
-            this.btCadastro.UseVisualStyleBackColor = true;
+            this.btCadastro.UseVisualStyleBackColor = false;
             this.btCadastro.Click += new System.EventHandler(this.btCadastro_Click);
             // 
             // btSair
             // 
-            this.btSair.Location = new System.Drawing.Point(217, 274);
+            this.btSair.BackColor = System.Drawing.Color.White;
+            this.btSair.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btSair.Location = new System.Drawing.Point(183, 275);
             this.btSair.Name = "btSair";
             this.btSair.Size = new System.Drawing.Size(75, 23);
             this.btSair.TabIndex = 6;
             this.btSair.Text = "Sair";
-            this.btSair.UseVisualStyleBackColor = true;
+            this.btSair.UseVisualStyleBackColor = false;
             this.btSair.Click += new System.EventHandler(this.btSair_Click);
             // 
             // btEntrar
             // 
-            this.btEntrar.Location = new System.Drawing.Point(55, 274);
+            this.btEntrar.BackColor = System.Drawing.Color.White;
+            this.btEntrar.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btEntrar.Location = new System.Drawing.Point(21, 275);
             this.btEntrar.Name = "btEntrar";
             this.btEntrar.Size = new System.Drawing.Size(75, 23);
             this.btEntrar.TabIndex = 7;
             this.btEntrar.Text = "Entrar";
-            this.btEntrar.UseVisualStyleBackColor = true;
+            this.btEntrar.UseVisualStyleBackColor = false;
             this.btEntrar.Click += new System.EventHandler(this.btEntrar_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lbFacaLogin);
+            this.panel1.Controls.Add(this.btSair);
+            this.panel1.Controls.Add(this.btEntrar);
+            this.panel1.Controls.Add(this.btCadastro);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.tbLogin);
+            this.panel1.Controls.Add(this.lbLogin);
+            this.panel1.Controls.Add(this.lbSenha);
+            this.panel1.Controls.Add(this.tbSenha);
+            this.panel1.Location = new System.Drawing.Point(29, 25);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(371, 329);
+            this.panel1.TabIndex = 8;
+            // 
+            // lbFacaLogin
+            // 
+            this.lbFacaLogin.AutoSize = true;
+            this.lbFacaLogin.Font = new System.Drawing.Font("Arial Black", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbFacaLogin.Location = new System.Drawing.Point(130, 16);
+            this.lbFacaLogin.Name = "lbFacaLogin";
+            this.lbFacaLogin.Size = new System.Drawing.Size(172, 27);
+            this.lbFacaLogin.TabIndex = 1;
+            this.lbFacaLogin.Text = "Faça seu Login";
             // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.ClientSize = new System.Drawing.Size(425, 351);
-            this.Controls.Add(this.btEntrar);
-            this.Controls.Add(this.btSair);
-            this.Controls.Add(this.btCadastro);
-            this.Controls.Add(this.tbSenha);
-            this.Controls.Add(this.tbLogin);
-            this.Controls.Add(this.lbSenha);
-            this.Controls.Add(this.lbLogin);
-            this.Controls.Add(this.pictureBox1);
+            this.ClientSize = new System.Drawing.Size(489, 477);
+            this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Login";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -148,6 +179,8 @@
         private System.Windows.Forms.Button btCadastro;
         private System.Windows.Forms.Button btSair;
         private System.Windows.Forms.Button btEntrar;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lbFacaLogin;
     }
 }
 
