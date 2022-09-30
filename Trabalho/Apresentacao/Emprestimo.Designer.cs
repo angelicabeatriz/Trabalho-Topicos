d01654aa683b5b73b59deecff453ee17a00a0bb9
Beatriz Angélica <92961502+angelicabeatriz@users.noreply.github.com>
2022-09-29 22:49:04 -0300

    Tela de Emprestimo

---
+++ Trabalho/Apresentacao/Emprestimo.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace Trabalho.Apresentacao
+{
+    partial class Emprestimo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btSair = new System.Windows.Forms.Button();
+            this.lbTtLivDisp = new System.Windows.Forms.Label();
+            this.tbQuantEx = new System.Windows.Forms.TextBox();
+            this.lbQuantEx = new System.Windows.Forms.Label();
+            this.tbTtLivDisp = new System.Windows.Forms.TextBox();
+            this.lbMatLiv = new System.Windows.Forms.Label();
+            this.lbTtLivEmp = new System.Windows.Forms.Label();
+            this.tbTtLivEmp = new System.Windows.Forms.TextBox();
+            this.tbPgEmp = new System.Windows.Forms.Button();
+            this.lbCadLiv = new System.Windows.Forms.Label();
+            this.btAnterior = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btBuscarLiv = new System.Windows.Forms.Button();
+            this.btDevLiv = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btSair
+            // 
+            this.btSair.Location = new System.Drawing.Point(332, 3);
+            this.btSair.Name = "btSair";
+            this.btSair.Size = new System.Drawing.Size(41, 23);
+            this.btSair.TabIndex = 55;
+            this.btSair.Text = "Sair";
+            this.btSair.UseVisualStyleBackColor = true;
+            // 
+            // lbTtLivDisp
+            // 
+            this.lbTtLivDisp.AutoSize = true;
+            this.lbTtLivDisp.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTtLivDisp.Location = new System.Drawing.Point(20, 272);
+            this.lbTtLivDisp.Name = "lbTtLivDisp";
+            this.lbTtLivDisp.Size = new System.Drawing.Size(151, 16);
+            this.lbTtLivDisp.TabIndex = 58;
+            this.lbTtLivDisp.Text = "Total de Livros Diponiveis";
+            // 
+            // tbQuantEx
+            // 
+            this.tbQuantEx.Location = new System.Drawing.Point(23, 165);
+            this.tbQuantEx.Name = "tbQuantEx";
+            this.tbQuantEx.Size = new System.Drawing.Size(236, 20);
+            this.tbQuantEx.TabIndex = 59;
+            // 
+            // lbQuantEx
+            // 
+            this.lbQuantEx.AutoSize = true;
+            this.lbQuantEx.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbQuantEx.Location = new System.Drawing.Point(20, 146);
+            this.lbQuantEx.Name = "lbQuantEx";
+            this.lbQuantEx.Size = new System.Drawing.Size(164, 16);
+            this.lbQuantEx.TabIndex = 60;
+            this.lbQuantEx.Text = "Quantidade de Exemplares";
+            // 
+            // tbTtLivDisp
+            // 
+            this.tbTtLivDisp.Location = new System.Drawing.Point(23, 291);
+            this.tbTtLivDisp.Name = "tbTtLivDisp";
+            this.tbTtLivDisp.Size = new System.Drawing.Size(236, 20);
+            this.tbTtLivDisp.TabIndex = 61;
+            // 
+            // lbMatLiv
+            // 
+            this.lbMatLiv.AutoSize = true;
+            this.lbMatLiv.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbMatLiv.Location = new System.Drawing.Point(20, 100);
+            this.lbMatLiv.Name = "lbMatLiv";
+            this.lbMatLiv.Size = new System.Drawing.Size(109, 16);
+            this.lbMatLiv.TabIndex = 62;
+            this.lbMatLiv.Text = "Livros Disponiveis";
+            // 
+            // lbTtLivEmp
+            // 
+            this.lbTtLivEmp.AutoSize = true;
+            this.lbTtLivEmp.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTtLivEmp.Location = new System.Drawing.Point(20, 209);
+            this.lbTtLivEmp.Name = "lbTtLivEmp";
+            this.lbTtLivEmp.Size = new System.Drawing.Size(170, 16);
+            this.lbTtLivEmp.TabIndex = 64;
+            this.lbTtLivEmp.Text = "Total de Livros Emprestados";
+            // 
+            // tbTtLivEmp
+            // 
+            this.tbTtLivEmp.Location = new System.Drawing.Point(23, 228);
+            this.tbTtLivEmp.Name = "tbTtLivEmp";
+            this.tbTtLivEmp.Size = new System.Drawing.Size(236, 20);
+            this.tbTtLivEmp.TabIndex = 65;
+            // 
+            // tbPgEmp
+            // 
+            this.tbPgEmp.Location = new System.Drawing.Point(23, 341);
+            this.tbPgEmp.Name = "tbPgEmp";
+            this.tbPgEmp.Size = new System.Drawing.Size(106, 23);
+            this.tbPgEmp.TabIndex = 66;
+            this.tbPgEmp.Text = "Pegar Emprestado";
+            this.tbPgEmp.UseVisualStyleBackColor = true;
+            // 
+            // lbCadLiv
+            // 
+            this.lbCadLiv.AutoSize = true;
+            this.lbCadLiv.Font = new System.Drawing.Font("Arial Black", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbCadLiv.Location = new System.Drawing.Point(40, 21);
+            this.lbCadLiv.Name = "lbCadLiv";
+            this.lbCadLiv.Size = new System.Drawing.Size(239, 27);
+            this.lbCadLiv.TabIndex = 67;
+            this.lbCadLiv.Text = "Emprestimo de Livros";
+            // 
+            // btAnterior
+            // 
+            this.btAnterior.Location = new System.Drawing.Point(3, 399);
+            this.btAnterior.Name = "btAnterior";
+            this.btAnterior.Size = new System.Drawing.Size(53, 23);
+            this.btAnterior.TabIndex = 68;
+            this.btAnterior.Text = "Anterior";
+            this.btAnterior.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btDevLiv);
+            this.panel1.Controls.Add(this.btBuscarLiv);
+            this.panel1.Controls.Add(this.btAnterior);
+            this.panel1.Controls.Add(this.lbCadLiv);
+            this.panel1.Controls.Add(this.tbPgEmp);
+            this.panel1.Controls.Add(this.tbTtLivEmp);
+            this.panel1.Controls.Add(this.lbTtLivEmp);
+            this.panel1.Controls.Add(this.lbMatLiv);
+            this.panel1.Controls.Add(this.tbTtLivDisp);
+            this.panel1.Controls.Add(this.lbQuantEx);
+            this.panel1.Controls.Add(this.tbQuantEx);
+            this.panel1.Controls.Add(this.lbTtLivDisp);
+            this.panel1.Controls.Add(this.btSair);
+            this.panel1.Location = new System.Drawing.Point(21, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(389, 458);
+            this.panel1.TabIndex = 0;
+            // 
+            // btBuscarLiv
+            // 
+            this.btBuscarLiv.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btBuscarLiv.Location = new System.Drawing.Point(184, 98);
+            this.btBuscarLiv.Name = "btBuscarLiv";
+            this.btBuscarLiv.Size = new System.Drawing.Size(75, 23);
+            this.btBuscarLiv.TabIndex = 69;
+            this.btBuscarLiv.Text = "Buscar Livro";
+            this.btBuscarLiv.UseVisualStyleBackColor = true;
+            // 
+            // btDevLiv
+            // 
+            this.btDevLiv.Location = new System.Drawing.Point(153, 341);
+            this.btDevLiv.Name = "btDevLiv";
+            this.btDevLiv.Size = new System.Drawing.Size(106, 23);
+            this.btDevLiv.TabIndex = 70;
+            this.btDevLiv.Text = "Devolver Livro";
+            this.btDevLiv.UseVisualStyleBackColor = true;
+            // 
+            // Emprestimo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1267, 707);
+            this.Controls.Add(this.panel1);
+            this.Name = "Emprestimo";
+            this.Text = "Emprestimo";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btSair;
+        private System.Windows.Forms.Label lbTtLivDisp;
+        private System.Windows.Forms.TextBox tbQuantEx;
+        private System.Windows.Forms.Label lbQuantEx;
+        private System.Windows.Forms.TextBox tbTtLivDisp;
+        private System.Windows.Forms.Label lbMatLiv;
+        private System.Windows.Forms.Label lbTtLivEmp;
+        private System.Windows.Forms.TextBox tbTtLivEmp;
+        private System.Windows.Forms.Button tbPgEmp;
+        private System.Windows.Forms.Label lbCadLiv;
+        private System.Windows.Forms.Button btAnterior;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btDevLiv;
+        private System.Windows.Forms.Button btBuscarLiv;
+    }
+}
